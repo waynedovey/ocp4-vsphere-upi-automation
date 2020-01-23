9cc5d9a8a990f798983aed82d6e236d3951ee054
Wayne Dovey <wdovey@gmail.com>
2020-01-23 19:00:19 +0000

    cleanup

--- cluster-build.sh
+++ cluster-build.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 rm -fr install-dir
-mkdir install-dir
+rm -fr bin
+rm -fr downloads
+mkdir -p install-dir
+mkdir -p bin
+mkdir -p downloads
 ansible-playbook -e @vars.yml setup-ocp-vsphere.yml --vault-password-file=../ocp4-vsphere-upi-automation-vault.yml
 
 scp install-dir/bootstrap.ign root@192.168.0.2:/var/www/html/ignition
