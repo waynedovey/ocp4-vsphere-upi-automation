5e3f3022b9c504c6f1bc4cd95f54cc8f737bd68b
Wayne Dovey <wdovey@gmail.com>
2020-01-30 05:49:32 +0000

    post install storage and bump specs

--- postinstall.sh
+++ postinstall.sh
@@ -9,3 +9,11 @@ oc create secret generic htpass-secret --from-file=htpasswd=./postinstall/users.
 oc apply -f ./postinstall/HTPasswd.yml
 oc adm policy add-cluster-role-to-user cluster-admin admin
 
+# OCS Storage 
+oc label node/storage0.ocp4.lab.gsslab.pek2.redhat.com role=storage-node
+oc label node/storage1.ocp4.lab.gsslab.pek2.redhat.com role=storage-node
+oc label node/storage2.ocp4.lab.gsslab.pek2.redhat.com role=storage-node
+
+oc create -f postinstall/rhocs-namespace.yaml
+oc create -f postinstall/rhocs-operatorgroup.yaml
+oc project openshift-storage
