042732809918085bde8c7404bc234fef72ba6952
Lucian Maly <lucian@redhat.com>
2020-03-10 11:44:19 +1100

    Moving vars to a separate folder

--- cluster-build-gsslab.sh
+++ cluster-build-gsslab.sh
@@ -5,11 +5,11 @@ rm -fr install-dir bin downloads
 mkdir -p {install-dir,bin,downloads}
 
 # Run Ansible setup-ocp-vsphere playbook:
-ansible-playbook -e @vars-gsslab.yml setup-ocp-vsphere.yml --vault-password-file=../ocp4-vsphere-upi-automation-vault.yml
+ansible-playbook -e @./vars/vars-gsslab.yml setup-ocp-vsphere.yml --vault-password-file=ocp4-vsphere-upi-automation-vault.yml
 
 # Copy Ignition file to the Apache's ignition folder under DocumentRoot:
 cp install-dir/bootstrap.ign /var/www/html/ignition
 chmod 644 /var/www/html/ignition/bootstrap.ign
 
 # Run Ansible setup-vcenter-vms playbook:
-ansible-playbook -e @vars-gsslab.yml setup-vcenter-vms.yml --vault-password-file=../ocp4-vsphere-upi-automation-vault.yml
+ansible-playbook -e @./vars/vars-gsslab.yml setup-vcenter-vms.yml --vault-password-file=ocp4-vsphere-upi-automation-vault.yml
