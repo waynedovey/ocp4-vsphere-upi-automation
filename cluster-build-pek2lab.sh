40469b3272c97c90bd32b4976da20aa543d9ffde
Wayne Dovey <wdovey@gmail.com>
2020-03-26 20:20:24 +1100

    cleanup and OCS setting

--- cluster-build-pek2lab.sh
+++ cluster-build-pek2lab.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
 DEFAULT_OCPVERSION=4.3.8
-DEFAULT_CLUSTERSIZE=small
+CLUSTER_SIZE=small
 WORKER_MEMORY=8192
 WORKER_CPU=2
 DISCONNECTED=false
+OCS_SETTING=false
+BUILD_LAB=pek2lab
 
 # Cleanup:
-#rm -fr install-dir bin downloads
-#mkdir -p {install-dir,bin,downloads}
+rm -fr install-dir bin downloads
+mkdir -p {install-dir,bin,downloads}
 
 # Set the OCP version
 if [ "$1" != "--silent" ]; then
@@ -22,7 +24,7 @@ printf "* Using: ${DEFAULT_OCPVERSION}\n\n"
 
 # Set Cluster size
 if [ "$1" != "--silent" ]; then
-    printf "Enter OpenShift Cluster Size (small [8gb,2vcpu],medium [32gb,4vcpu],large [64gb,8vcpu]): (Press ENTER for default: ${DEFAULT_CLUSTERSIZE} ${SMALL_CLUSTER})\n"
+    printf "Enter OpenShift Cluster Size (small [8gb,2vcpu],medium [32gb,4vcpu],large [64gb,8vcpu]): (Press ENTER for default: ${CLUSTER_SIZE} ${SMALL_CLUSTER})\n"
     read -r CLUSTER_SIZE
     if [ "${CLUSTER_SIZE}" == "medium" ]; then
         WORKER_MEMORY=32768
@@ -31,7 +33,7 @@ if [ "$1" != "--silent" ]; then
         WORKER_MEMORY=65536
         WORKER_CPU=8
     elif [ "${CLUSTER_SIZE}" != "" ]; then
-        CLUSTER_SIZE=${DEFAULT_CLUSTERSIZE};
+        CLUSTER_SIZE=${CLUSTER_SIZE};
         WORKER_MEMORY=8192
         WORKER_CPU=2
     fi
@@ -40,7 +42,7 @@ printf "* Using: ${CLUSTER_SIZE} Cluster Settings Memory ${WORKER_MEMORY} CPU ${
 
 # Disconnected
 if [ "$1" != "--silent" ]; then
-    printf "Enter OpenShift Disconnected setting true/false: (Press ENTER for default: ${DEFAULT_DISCONNECTED})\n"
+    printf "Enter OpenShift Disconnected setting true/false: (Press ENTER for default: ${DISCONNECTED})\n"
     read -r DISCONNECTED_CHOICE
     if [ "${DISCONNECTED_CHOICE}" == "true" ]; then
         DISCONNECTED=true
@@ -48,16 +50,30 @@ if [ "$1" != "--silent" ]; then
         DISCONNECTED=false
     elif [ "${DISCONNECTED_CHOICE}" != "" ]; then
         DISCONNECTED=false
-    fi    
+    fi
 fi
 printf "* Disconnected Setting: ${DISCONNECTED}\n\n"
 
+# Disconnected
+if [ "$1" != "--silent" ]; then
+    printf "Enable OpenShift Container Storage (OCS) true/false: (Press ENTER for default: ${OCS_SETTING})\n"
+    read -r OCS_CHOICE
+    if [ "${OCS_CHOICE}" == "true" ]; then
+        OCS_SETTING=true
+    elif [ "${DISCONNECTED_CHOICE}" == "false" ]; then
+        OCS_SETTING=false
+    elif [ "${DISCONNECTED_CHOICE}" != "" ]; then
+        OCS_SETTING=false
+    fi
+fi
+printf "* OpenShift Container Storage (OCS) Setting: ${OCS_SETTING}\n\n"
+
 # Run Ansible setup-ocp-vsphere playbook:
-#ansible-playbook -e "ocp_version=${DEFAULT_OCPVERSION}" -e @./vars/vars-pek2lab.yml setup-ocp-vsphere.yml --vault-password-file=ocp4-vsphere-upi-automation-vault.yml --skip-tags=2
+ansible-playbook -e "ocp_version=${DEFAULT_OCPVERSION}" -e @./vars/vars-${BUILD_LAB}.yml setup-ocp-vsphere.yml --vault-password-file=ocp4-vsphere-upi-automation-vault.yml
 
 # Copy Ignition file to the Apache's ignition folder under DocumentRoot:
-#cp install-dir/bootstrap.ign /var/www/html/ignition
-#chmod 644 /var/www/html/ignition/bootstrap.ign
+cp install-dir/bootstrap.ign /var/www/html/ignition
+chmod 644 /var/www/html/ignition/bootstrap.ign
 
 # Run Ansible setup-vcenter-vms playbook:
-#ansible-playbook -e "ocp_version=${DEFAULT_OCPVERSION} worker_memory=${WORKER_MEMORY} worker_cpu=${WORKER_CPU}"  -e @./vars/vars-pek2lab.yml setup-vcenter-vms.yml --vault-password-file=ocp4-vsphere-upi-automation-vault.yml -vvv
+ansible-playbook -e "ocp_version=${DEFAULT_OCPVERSION} worker_memory=${WORKER_MEMORY} worker_cpu=${WORKER_CPU} disconnected_setting=${DISCONNECTED}" -e @./vars/vars-${BUILD_LAB}.yml setup-vcenter-vms.yml --vault-password-file=ocp4-vsphere-upi-automation-vault.yml
