68eb12fd808edfe6b7abdf470d246884fe7a3148
Wayne Dovey <wdovey@gmail.com>
2020-04-03 06:48:37 +0000

    cluster status enabled

--- cluster-build.sh
+++ cluster-build.sh
@@ -79,11 +79,11 @@ fi
 printf "* OpenShift Container Storage (OCS) Setting: ${OCS_SETTING}\n\n"
 
 # Run Ansible setup-ocp-vsphere playbook:
-ansible-playbook -e "ocp_version=${DEFAULT_OCPVERSION} disconnected_setting=${DISCONNECTED}" -e @./vars/vars-${BUILD_LAB}.yml setup-ocp-vsphere.yml --vault-password-file=ocp4-vsphere-upi-automation-vault.yml --skip-tags=2
+ansible-playbook -e "ocp_version=${DEFAULT_OCPVERSION} disconnected_setting=${DISCONNECTED} BUILD_LAB=${BUILD_LAB}" -e @./vars/vars-${BUILD_LAB}.yml setup-ocp-vsphere.yml --vault-password-file=ocp4-vsphere-upi-automation-vault.yml --skip-tags=2
 
 # Copy Ignition file to the Apache's ignition folder under DocumentRoot:
 cp install-dir/bootstrap.ign /var/www/html/ignition
 chmod 644 /var/www/html/ignition/bootstrap.ign
 
 # Run Ansible setup-vcenter-vms playbook:
-ansible-playbook -e "ocp_version=${DEFAULT_OCPVERSION} worker_memory=${WORKER_MEMORY} worker_cpu=${WORKER_CPU} disconnected_setting=${DISCONNECTED}" -e @./vars/vars-${BUILD_LAB}.yml setup-vcenter-vms.yml --vault-password-file=ocp4-vsphere-upi-automation-vault.yml
+ansible-playbook -e "ocp_version=${DEFAULT_OCPVERSION} worker_memory=${WORKER_MEMORY} worker_cpu=${WORKER_CPU} disconnected_setting=${DISCONNECTED} BUILD_LAB=${BUILD_LAB}" -e @./vars/vars-${BUILD_LAB}.yml setup-vcenter-vms.yml --vault-password-file=ocp4-vsphere-upi-automation-vault.yml
